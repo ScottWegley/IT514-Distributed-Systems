fcbb01fcbf3c4a786250a0bd0e3ebc7574a0fa11
Scott Wegley <scbrwegley@protonmail.ch>
2024-11-24 12:39:54 -0800

    week 1 practical code assignment

---
+++ Week_1/HelloWorld.java
@@ -0,0 +1,21 @@
+package Week_1;
+
+class HelloWorld {
+
+    /**
+     * Prints the traditional "Hello, World!" message to the console.
+     * Additionally print my name to the console in the form of ASCII art.
+     * 
+     * @author Scott Wegley
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("Hello, World!");
+        System.out.println(" __           _   _   ");
+        System.out.println("/ _\\ ___ ___ | |_| |_ ");
+        System.out.println("\\ \\ / __/ _ \\| __| __|");
+        System.out.println("_\\ \\ (_| (_) | |_| |_ ");
+        System.out.println("\\__/\\___\\___/ \\__|\\__|");
+    }
+}
