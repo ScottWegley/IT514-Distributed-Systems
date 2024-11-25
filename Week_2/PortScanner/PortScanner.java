37899a41ecf1eb13a46b9d95c89c5a192e73c163
Scott Wegley <scbrwegley@protonmail.ch>
2024-11-24 16:36:56 -0800

    added debug print out to verify program is still running for long scans

--- Week_2/PortScanner/PortScanner.java
+++ Week_2/PortScanner/PortScanner.java
@@ -13,10 +13,16 @@ class PortScanner {
     public static void main(String[] args) {
         String hostname = args[0];
         System.out.println("Scanning ports on host: " + hostname);
+        int portsSinceLastPrint = 0;
         for (int port = 1; port <= 65535; port++) {
             try {
+                portsSinceLastPrint++;
+                if (portsSinceLastPrint == 1000) {
+                    System.out.println("Scanned ports 1-" + port);
+                    portsSinceLastPrint = 0;
+                }
                 Socket socket = new Socket();
-                socket.connect(new InetSocketAddress(hostname, port), 1000);
+                socket.connect(new InetSocketAddress(hostname, port), 500);
                 socket.close();
                 System.out.println("Port " + port + " is open");
             } catch (Exception e) {
